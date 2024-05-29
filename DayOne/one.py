5245e95c01bd4fb00852d2f4fb1f65c7a9610dbd
Master-Bibash <dokfreak142@gmail.com>
2024-05-29 21:46:44 +0545

    day one py

---
+++ DayOne/one.py
@@ -0,0 +1,24 @@
+# making an empty disctionary 
+
+file={
+     'age':22
+};
+file.update(
+     {
+          "name":"bibash",
+          'gender':"male",
+     }
+),
+
+print(file['gender']);
+
+## loop examples
+number=[1,2,3,4,5,6,7,8];
+for num in number:
+     print("num is"+str(num));
+
+i=1;
+while i<=6:
+     print(i)
+     i+=i;     
+

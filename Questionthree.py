90ec91852ee7a12b9d5ffd3159c68b99d8bb9639
Master-Bibash <dokfreak142@gmail.com>
2024-07-05 21:48:54 +0545

    master

---
+++ Questionthree.py
@@ -0,0 +1,24 @@
+# functaion takes agrument tht tkaes number if true even or false if odd
+# function takes argument takes string and return the reverse of string
+# function that takes string and retrurns length
+# function takes string as argument and treturns the fitst letter/char fo string
+# functuoin taht takesa a string and return true if the first char is upper or false is returned
+# function takes a string as argu that returns a last char of string
+# function takes a string and checks whether degit is present in the string
+# function takes a string that checks palindrome disregarding case sensitivity
+# write a function that takes a string as an arguent and returna the total number of or count of the letter you present in the string without using count
+# write a fucntion tht takesa string as argument and checks whether the letter u is present in the strinf disregarinf case sensitivity
+# write a function that takes a list of numbers as an arguent and retuerns the list of number containing even numbers
+# function taeks any number of arghuments and returns maximum of thhose of nnumbers
+# (1,23,4) return 4
+# function that takes a string as an args and check if vowels are present in the string or not
+# function that takes a string as an args and checks if consonants are present in the string or not
+# function that takes a string as an args and returns the first vowels present in the string
+# function that takes a string and returns the first consonenent int hat stirng
+# function that takes a string as an args and return the total number of vowels present in the string
+# function that  takes string and return number of consonents present in the string
+# funtion that takesa  number and returns the list of even number upto that number without using range
+# funtion that takes a number as an argument and returns list of even adnd odd numbers upto that nyumber
+# 10
+# [1,3,5,7,9]
+# [2,4,6,8,10]
